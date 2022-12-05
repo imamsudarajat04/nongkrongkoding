21a41095d08e964357335f7e825c46a9625733c4
falfikri <fazliialfikri@gmail.com>
2022-12-06 05:00:17 +0700

    create admin page & user page(null)

---
+++ resources/views/includes/styleadmin.blade.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+<meta name="description" content="" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+rel="stylesheet"
+/>
+
+<link rel="icon" type="image/x-icon" href="{{ asset('admin/assets/img/favicon/favicon.ico') }}"/>
+
+<link rel="stylesheet" href="{{ asset('admin/assets/vendor/fonts/boxicons.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('admin/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+<link rel="stylesheet" href="{{ asset('admin/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+<link rel="stylesheet" href="{{ asset('admin/assets/css/demo.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('admin/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+
+<script src="{{ asset('admin/assets/vendor/js/helpers.js') }}"></script>
+
+<script src="{{ asset('admin/assets/js/config.js') }}"></script>
+
+<title>@yield('adminTitle')</title>
---
+++ resources/views/layouts/adminlayout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('includes.styleadmin')
+</head>
+<body>
+    <div class="layout-wrapper layout-content-navbar">
+
+        <div class="layout-container">
+
+            @include('components.adminside')
+
+            <div class="layout-page">
+
+                @include('components.adminnav')
+
+                <div class="content-wrapper">
+
+                    @yield('adminContent')
+
+                    @include('components.adminfooter')
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('includes.scriptadmin')
+</body>
+
+</html>
