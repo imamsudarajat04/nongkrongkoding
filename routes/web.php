b10082ad240c5abcb76d4ab431d6da04efa49df2
imamsudarajat04 <imamsudarajat708@gmail.com>
2022-12-06 20:27:56 +0700

    route untuk landingpage, login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LandingPageController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Landing Page
+Route::get('/', [LandingPageController::class, 'index'])->name('landingPage.index');
 
-// Test Admin Page
-// Route::get('/', function () {
-//     return view('welcome');
+//Auth Login
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+
+//Testing Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+// Route::group(['middleware' => ['auth']], function() {
+
+//     //Dashboard
+//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 // });
 
 
-Route::get('/dashboard', function () {
-    return view('layouts.adminlayout');
-});
-
-
-// Test User Page
-Route::get('/user', function () {
-    return view('layouts.userlayout');
-});
-
-
-// Test Login Page Custom
-Route::get('/login', function () {
-    return view('auth.master');
-});
+// // Test Login Page Custom
+// Route::get('/login', function () {
+//     return view('auth.master');
+// });
