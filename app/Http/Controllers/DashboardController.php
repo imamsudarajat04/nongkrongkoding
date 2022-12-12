d7c50a5826147cdbd222e55bd727304358cff269
falfikri <fazliialfikri@gmail.com>
2022-12-12 18:51:00 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('layouts.adminlayout');
+        $admin = DB::table('users')->where('id');
+        return view('dashboard.index', ['admin' => $admin]);
     }
 }
