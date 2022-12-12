88529afb285061ea11c7326a8f225f34a20efd91
imamsudarajat04 <imamsudarajat708@gmail.com>
2022-12-13 01:39:45 +0700

    Seeder Permission

---
+++ database/seeders/Permission.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class Permission extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Permission::create([
+            'name'       => 'system_management_access',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'user_access',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'admin_access',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'user_create',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'admin_create',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'user_edit',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'admin_edit',
+            'guard_name' => 'web'
+        ]);
+
+        \App\Models\Permission::create([
+            'name'       => 'system_management_access',
+            'guard_name' => 'web'
+        ]);
+    }
+}
