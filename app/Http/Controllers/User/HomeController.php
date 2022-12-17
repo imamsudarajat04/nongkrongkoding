82c7694b24b2253cf71e5bf7e1ee0b620b9d0adb
falfikri <fazliialfikri@gmail.com>
2022-12-17 19:36:10 +0700

    mengubah dan menambahkan icon pada dashboard

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class HomeController extends Controller
 {
-    //
+    
 }
