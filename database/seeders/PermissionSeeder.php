82c0d8a88efe43a76044354585ee55a4c875203a
imamsudarajat04 <imamsudarajat708@gmail.com>
2022-12-28 02:04:50 +0700

    change name permissionseeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class Permission extends Seeder
+class PermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
