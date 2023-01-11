5d6629b60f0a4a616ae730bb4e44e6a0f98abec8
falfikri <fazliialfikri@gmail.com>
2023-01-11 20:53:44 +0700

    layout template dashboard

---
+++ resources/views/dashboard/includes/styledashboard.blade.php
@@ -0,0 +1,25 @@
+
+    <meta charset="utf-8" />
+    <title>@yield('title', 'Dashboard')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- jsvectormap css -->
+    <link href="{!! asset('admin/assets/libs/jsvectormap/css/jsvectormap.min.css') !!}" rel="stylesheet" type="text/css" />
+
+    <!--Swiper slider css-->
+    <link href="{!! asset('admin/assets/libs/swiper/swiper-bundle.min.css') !!}}" rel="stylesheet" type="text/css" />
+
+    <!-- Layout config Js -->
+    <script src="{!! asset('admin/assets/js/layout.js') !!}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{!! asset('admin/assets/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{!! asset('admin/assets/css/icons.min.css') !!}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{!! asset('admin/assets/css/app.min.css') !!}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{!! asset('admin/assets/css/custom.min.css') !!}" rel="stylesheet" type="text/css" />
